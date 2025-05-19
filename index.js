8a330c273a60aa478c05dc54df72a767008456f4
NikoSAN02 <dineshbohra@gmail.com>
2025-05-20 02:21:12 +0530

    updated for cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const ethers = require('ethers');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
